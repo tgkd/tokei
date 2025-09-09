56adfb71ab30630ae4bbf78482f728fde7cffd10
tgkd <p.y.trofimov@gmail.com>
2025-09-09 11:33:22 +0200

    Initial Commit

---
+++ tokei/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  tokei
+//
+//  Created by P on 9/9/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ tokei/tokeiApp.swift
@@ -0,0 +1,17 @@
+//
+//  tokeiApp.swift
+//  tokei
+//
+//  Created by P on 9/9/25.
+//
+
+import SwiftUI
+
+@main
+struct tokeiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
